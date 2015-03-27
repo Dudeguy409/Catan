50d7cf099357256dab3ebb93eccafc7f4997b7e1
Ethan Campbell <campbeeg@rose-hulman.edu>
2015-03-27 15:42:13 -0400

    Added structure and road test

--- test/test.java
+++ test/test.java
@@ -16,5 +16,16 @@ public class test {
 		Die die = new Die();
 		assertTrue(die.rollDie()<7);
 	}
+	
+	@Test
+	public void testRoadExists() {
+		Road road = new Road(0, null, null);
+		assertNotNull(road);
+	}
 
+	@Test
+	public void testStructureGetPlayer() {
+		Structure struct = new Structure(1, 0, 0, 0, 0, 0, null);
+		assertEquals(1, struct.getPlayer());
+	}
 }
