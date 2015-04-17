00a13bf3e80ccbc6372e3984425bc30fa833ea09
Ethan Campbell <campbeeg@rose-hulman.edu>
2015-04-16 23:15:18 -0400

    Structure tests

--- test/StructurePieceTest.java
+++ test/StructurePieceTest.java
@@ -3,130 +3,118 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import client.Controller.RoadManager;
 import client.Controller.StructureManager;
 import client.Model.Structure;
 
 public class StructurePieceTest {
 	@Test
 	public void testStructureManagerInitializes() {
-		StructureManager rm = new StructureManager(3);
-		assertNotNull(rm);
+		StructureManager sm = new StructureManager(3);
+		assertNotNull(sm);
 	}
 	
 	@Test
 	public void testStructureMapInitializes() {
-		StructureManager rm = new StructureManager(3);
-		assertNotNull(rm.StructureDependencyMap);
+		StructureManager sm = new StructureManager(3);
+		assertNotNull(sm.StructureDependencyMap);
 	}
 	
 	@Test
 	public void testStructureMapCorrectSize() {
-		StructureManager rm = new StructureManager(3);
-		assertEquals(3, rm.StructureDependencyMap.size());
+		StructureManager sm = new StructureManager(3);
+		assertEquals(3, sm.StructureDependencyMap.size());
+	}
+	
+	@Test
+	public void testStructureMapDoesntAddsStructure() {
+		StructureManager sm = new StructureManager(3);
+		sm.addStructure(1, 5);
+		assertEquals(0, sm.getStructureCountForPlayer(1));
 	}
 	
 	@Test
 	public void testStructureMapAddsStructureToCorrectPlayer() {
-		StructureManager rm = new StructureManager(3);
-		rm.addStructurePieceAtBeginning(1, 5);
-		assertEquals(1, rm.getStructureCountForPlayer(1));
+		StructureManager sm = new StructureManager(3);
+		RoadManager rm = new RoadManager(3);
+		rm.addRoadPieceAtBeginning(1, 5);
+		sm.addStructure(1, 5);
+		assertEquals(1, sm.getStructureCountForPlayer(1));
 	}
 	
 	@Test(expected=IndexOutOfBoundsException.class)
 	public void testStructureMapThrowsExceptionWhenNonPlayerReferenced() {
-		StructureManager rm = new StructureManager(3);
-		rm.addStructurePiece(3, 5);
+		StructureManager sm = new StructureManager(3);
+		RoadManager rm = new RoadManager(3);
+		rm.addRoadPieceAtBeginning(1, 5);
+		sm.addStructure(3, 5);
 	}
 	
 	@Test(expected=IndexOutOfBoundsException.class)
 	public void testStructureMapThrowsExceptionWhenNonLocationReferenced() {
-		StructureManager rm = new StructureManager(3);
-		rm.addStructurePiece(2, -1);
-	}
-
-	@Test
-	public void testCorrectMapSizes() {
-		int rslt;
-		StructureManager rm = new StructureManager(3);
-		rslt = rm.getStructureCountForPlayer(0);
-		assertEquals(0, rslt);
-
-		rm.addStructurePiece(0, 19);
-
-		rslt = rm.getStructureCountForPlayer(0);
-		assertEquals(1, rslt);
-
-		rm.addStructurePiece(0, 20);
-		rm.addStructurePiece(0, 24);
-		rm.addStructurePiece(0, 30);
-		rm.addStructurePiece(0, 1);
-
-		rslt = rm.getStructureCountForPlayer(0);
-		assertEquals(5, rslt);
+		StructureManager sm = new StructureManager(3);
+		RoadManager rm = new RoadManager(3);
+		rm.addRoadPieceAtBeginning(1, 5);
+		sm.addStructure(2, -1);
 	}
 	
 	@Test
 	public void testAddOneStructureAtBeginning() {
 		int rslt;
-		StructureManager rm = new StructureManager(3);
-		rslt = rm.getStructureCountForPlayer(0);
+		StructureManager sm = new StructureManager(3);
+		rslt = sm.getStructureCountForPlayer(0);
 		assertEquals(0, rslt);
+		
+		RoadManager rm = new RoadManager(3);
+		rm.addRoadPieceAtBeginning(0, 27);
+		sm.addStructure(0, 19);
 
-		rm.addStructurePieceAtBeginning(0, 19);
-
-		rslt = rm.getStructureCountForPlayer(0);
+		rslt = sm.getStructureCountForPlayer(0);
 		assertEquals(1, rslt);
-	}
-
-	@Test
-	public void testAddOneStructure() {
-		int rslt;
-		StructureManager rm = new StructureManager(3);
-		rslt = rm.getStructureCountForPlayer(0);
-		assertEquals(0, rslt);
-
-		rm.addStructurePieceAtBeginning(0, 14);
-		rm.addStructurePiece(0, 19);
-
-		rslt = rm.getStructureCountForPlayer(0);
-		assertEquals(2, rslt);
 	}
 
 	@Test
 	public void testAddTwoSeparate() {
 		int rslt;
-		StructureManager rm = new StructureManager(3);
-		rm.addStructurePieceAtBeginning(0, 19);
-		rm.addStructurePieceAtBeginning(0, 51);
+		StructureManager sm = new StructureManager(3);
+		
+		RoadManager rm = new RoadManager(3);
+		rm.addRoadPieceAtBeginning(0, 27);
+		sm.addStructure(0, 19);
+		
+		rm.addRoadPieceAtBeginning(0, 66);
+		sm.addStructure(0, 51);
 
-		rslt = rm.getStructureCountForPlayer(0);
-		assertEquals(1, rslt);
+		rslt = sm.getStructureCountForPlayer(0);
+		assertEquals(2, rslt);
 	}
 
 	@Test
 	public void testAddTwoAdjacent() {
 		int rslt;
-		StructureManager rm = new StructureManager(3);
-		rm.addStructurePieceAtBeginning(0, 19);
-		rm.addStructurePiece(0, 22);
+		StructureManager sm = new StructureManager(3);
+		RoadManager rm = new RoadManager(3);
+		rm.addRoadPieceAtBeginning(0, 27);
+		sm.addStructure(0, 19);
+		sm.addStructure(0, 20);
 
-		rslt = rm.getStructureCountForPlayer(0);
-		assertEquals(2, rslt);
+		rslt = sm.getStructureCountForPlayer(0);
+		assertEquals(1, rslt);
 	}
 
 	@Test
-	public void testLoop() {
+	public void testOpponentAddAdjacent() {
 		int rslt;
-		StructureManager rm = new StructureManager(3);
-		rm.addStructurePieceAtBeginning(0, 19);
-		rm.addStructurePiece(0, 22);
-		rm.addStructurePiece(0, 31);
-		rm.addStructurePiece(0, 36);
-		rm.addStructurePiece(0, 32);
-		rm.addStructurePiece(0, 23);
+		StructureManager sm = new StructureManager(3);
+		RoadManager rm = new RoadManager(3);
+		rm.addRoadPieceAtBeginning(0, 27);
+		sm.addStructure(0, 19);
+		
+		rm.addRoadPieceAtBeginning(1, 31);
+		sm.addStructure(1, 20);
 
-		rslt = rm.getStructureCountForPlayer(0);
-		assertEquals(6, rslt);
+		rslt = sm.getStructureCountForPlayer(1);
+		assertEquals(0, rslt);
 	}
 
 }
