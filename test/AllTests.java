610e790c8404630a6de640c8645ce296d8d676db
Andrew Davidson <acdavidson@sbcglobal.net>
2015-04-17 15:38:24 -0400

    added a possibly functional testing suite???

---
+++ test/AllTests.java
@@ -0,0 +1,12 @@
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ test.class, GameTest.class, HexManagerTest.class,
+		StructurePieceTest.class, LongestRoadTest.class })
+public class AllTests {
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("AllTests");
+	}
+}
