99673085ff8adf57d480deb67147f61cec4b93f2
Ethan Campbell <campbeeg@rose-hulman.edu>
2015-04-16 23:24:23 -0400

    StructureManager constructor

--- src/client/Controller/StructureManager.java
+++ src/client/Controller/StructureManager.java
@@ -1,7 +1,24 @@
 package client.Controller;
 
-public class StructureManager {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import client.Model.Road;
+import client.Model.RoadPiece;
+import client.Model.Structure;
+
+public class StructureManager {
+	
+	public ArrayList<HashMap<Integer, StructurePiece>> structurePieceMaps;
+	public HashMap<Integer, Structure> structureDependencyMap;
+
+	public StructureManager(int playerCount) {
+		structurePieceMaps = new ArrayList<HashMap<Integer, StructurePiece>>();
+		for (int i = 0; i < playerCount; i++) {
+			structurePieceMaps.add(new HashMap<Integer, StructurePiece>());
+		}
+	}
+	
 	public void addStructure(int structureId, int playerNumber) throws Exception {
 		
 	}
