f476cb47b063b6f0f8d05a12c344b4cf059246e5
Andrew Davidson <acdavidson@sbcglobal.net>
2015-04-22 19:49:33 -0400

    added doc of what we are going to write tests for and fixed JOptionPanes
    added dependency injection to the game class so that we can pass in the
    number of players and the colors

--- src/client/Controller/Main.java
+++ src/client/Controller/Main.java
@@ -1,8 +1,10 @@
 package client.Controller;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import client.GUI.BoardRenderer;
 import client.GUI.UserPanel;
 
@@ -23,11 +25,18 @@ public class Main {
 	 *            Command-line arguments, ignored here
 	 */
 	public static void main(String[] args) {
+		int playerCount = configureNumberOfPlayers();
+		if (playerCount < 2) {
+			System.exit(0);
+		}
+
+		Color[] colors = configurePlayerColors(playerCount);
+
 		JFrame frame = new JFrame();
 		frame.setSize(SIZE);
 		frame.setTitle("Settlers of Catan");
 		frame.setLayout(new FlowLayout());
-		Game game = new Game();
+		Game game = new Game(playerCount, colors);
 		UserPanel myPanel = new UserPanel(game);
 		frame.add(myPanel, FlowLayout.LEFT);
 		BoardRenderer myBoard = new BoardRenderer(game);
@@ -35,4 +44,85 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
+
+	private static Color[] configurePlayerColors(int numberOfPlayers) {
+		Object[] options2 = { "Red", "Blue", "White", "Magenta", "Orange",
+				"Cyan" };
+		Color[] colorArray = new Color[numberOfPlayers];
+		for (int i = 0; i < numberOfPlayers; i++) {
+			boolean diffColor = false;
+			while (!diffColor) {
+				int color = JOptionPane.showOptionDialog(null,
+						"What color would you like, Player " + (i + 1) + "?",
+						"Setup", JOptionPane.DEFAULT_OPTION,
+						JOptionPane.QUESTION_MESSAGE, null, options2, null);
+				diffColor = true;
+				switch (color) {
+				case 0:
+					colorArray[i] = Color.red;
+					break;
+				case 1:
+					colorArray[i] = Color.blue;
+					break;
+				case 2:
+					colorArray[i] = Color.white;
+					break;
+				case 3:
+					colorArray[i] = Color.magenta;
+					break;
+				case 4:
+					colorArray[i] = Color.orange;
+					break;
+				case 5:
+					colorArray[i] = Color.cyan;
+					break;
+				case 6:
+					colorArray[i] = Color.black;
+					break;
+				case 7:
+					colorArray[i] = Color.red;
+					break;
+				default:
+					int rslt = JOptionPane
+							.showConfirmDialog(
+									null,
+									"You have selected to close the color menu.  This will quit the game.  Do you want to quit?",
+									"Exit Program?",
+									JOptionPane.OK_CANCEL_OPTION,
+									JOptionPane.WARNING_MESSAGE, null);
+					System.out.println(rslt);
+					if (rslt != 2) {
+						System.exit(0);
+					}else{
+						diffColor=false;
+					}
+				}
+
+				// verifies that the user selects a color that hasn't
+				// already been chosen.
+				for (int j = 0; j < i; j++) {
+					if (colorArray[i] == colorArray[j]) {
+						diffColor = false;
+						JOptionPane
+								.showConfirmDialog(
+										null,
+										"This color has already been chosen by another player.  Please choose a different one.");
+						break;
+					}
+
+				}
+
+			}
+		}
+		return colorArray;
+	}
+
+	private static int configureNumberOfPlayers() {
+		Object[] options1 = { "2", "3", "4" };
+		return JOptionPane.showOptionDialog(null,
+				"How many players are there?", "Setup",
+				JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+				options1, -1) + 2;
+	}
+
 }
