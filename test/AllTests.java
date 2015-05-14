edad34d5ae78f18d724df2430f77ecc04c11f5cf
Andrew Davidson <acdavidson@sbcglobal.net>
2015-05-14 15:49:47 -0400

    added new tests to testing suite

--- test/AllTests.java
+++ test/AllTests.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 @Suite.SuiteClasses({ test.class, GameTest.class, HexManagerTest.class,
 		StructurePieceTest.class, LongestRoadTest.class,
 		DevelopmentCardTest.class, GamePlayerTradeTest.class,
-		GameBankTradeTest.class })
+		GameBankTradeTest.class, HexResourceGeneratorTest.class,
+		PortGeneratorTest.class })
 public class AllTests {
 	public static void main(String args[]) {
 		org.junit.runner.JUnitCore.main("AllTests");
